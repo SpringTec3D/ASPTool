c643d758d53545286b8b902e3bab4d8bdf1e95d2
SpringTec3D <49334282+SpringTec3D@users.noreply.github.com>
2025-07-27 17:23:43 +0200

    Update Invoke-ASPTool.ps1

--- Invoke-ASPTool.ps1
+++ Invoke-ASPTool.ps1
@@ -1,1 +1,6 @@
-
+Invoke-WebRequest https://github.com/SpringTec3D/ASPTool/raw/refs/heads/main/ASPTool.zip -OutFile ASPTool.zip -Method Get
+Expand-Archive -Path ASPTool.zip -DestinationPath ASPTool
+Invoke-Expression ASPTool/RanSIM.exe
+Start-Sleep -Seconds 5
+Remove-Item -Path "ASPTool.zip" -Force
+Remove-Item -Path "ASPTool" -Recurse -Force
